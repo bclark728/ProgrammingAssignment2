bac34cf401c7997077484144a0e1b9e604727f6e
bclark728 <bryan.allan.clark@gmail.com>
2015-07-12 01:39:54 -0600

    working, undocumented version

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(trix = matrix()) {
+  trix.inv <- NULL
+  set <- function(y) {
+    tirx     <<- y
+    trix.inv <<- NULL
+  }
+  get <- function() trix
+  setinv <- function(inv) trx.inv <<- inv
+  getinv <- function() trix.inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    print("using cached")
+    return(inv)
+  }
+  trix <- x$get()
+  inv <- solve(trix, ...)
+  x$setinv(inv)
+  inv
 }
